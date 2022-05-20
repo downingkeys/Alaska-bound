7148b91ad4f9fa7bbacea0c3d00c4fb8510585a5
Timothy Downing <downingkeys@gmail.com>
2022-05-20 11:03:02 -0700

    Copy files from LE CSS as template

---
+++ _site/js/gallery.js
@@ -0,0 +1,32 @@
+// Activates the image gallery.
+/* The main task is to attach an event listener to each image in the gallery and respond appropriately on click. */
+function activateGallery() {
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage  = document.querySelector("#gallery-photo > img");
+  // Image info to be updated
+  let galleryInfo = document.querySelector("#gallery-info");
+  let title       = galleryInfo.querySelector(".title");
+  let description = galleryInfo.querySelector(".description");
+
+  thumbnails.forEach(function(thumbnail) {
+    // Preload large images.
+    let newImageSrc  = thumbnail.dataset.largeVersion;
+    let largeVersion = new Image();
+    largeVersion.src = newImageSrc;
+
+    thumbnail.addEventListener("click", function() {
+      // Set clicked image as main image.
+      mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", thumbnail.alt);
+
+
+      // Change which image is current.
+      document.querySelector(".current").classList.remove("current");
+      thumbnail.parentNode.classList.add("current");
+
+      // Update image info.
+      title.innerHTML       = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+    });
+  });
+}
